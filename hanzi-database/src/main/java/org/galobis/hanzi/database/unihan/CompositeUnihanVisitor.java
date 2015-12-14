9cf3040a86c7f618e8a94ba0410b256e6b2c2833
Aloisio Ribeiro <aloisio@users.noreply.github.com>
2015-12-13 21:01:24 -0500

    Introduce Composite pattern implementation for UnihanVisitor

---
+++ hanzi-database/src/main/java/org/galobis/hanzi/database/unihan/CompositeUnihanVisitor.java
@@ -0,0 +1,26 @@
+package org.galobis.hanzi.database.unihan;
+
+import org.galobis.hanzi.model.Hanzi;
+
+public class CompositeUnihanVisitor implements UnihanVisitor {
+
+    private final UnihanVisitor[] childVisitors;
+
+    public CompositeUnihanVisitor(UnihanVisitor... visitors) {
+        this.childVisitors = visitors;
+    }
+
+    @Override
+    public void visit(Hanzi hanzi) throws Exception {
+        for (UnihanVisitor visitor : childVisitors) {
+            visitor.visit(hanzi);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        for (UnihanVisitor visitor : childVisitors) {
+            visitor.close();
+        }
+    }
+}
---
+++ hanzi-database/src/test/java/org/galobis/hanzi/database/unihan/CompositeUnihanVisitorTest.java
@@ -0,0 +1,48 @@
+package org.galobis.hanzi.database.unihan;
+
+import org.galobis.hanzi.model.Hanzi;
+import org.junit.Before;
+import org.junit.Test;
+
+import mockit.Injectable;
+import mockit.Verifications;
+
+public class CompositeUnihanVisitorTest {
+    private CompositeUnihanVisitor visitor;
+
+    @Injectable
+    private UnihanVisitor visitor1;
+
+    @Injectable
+    private UnihanVisitor visitor2;
+
+    @Before
+    public void createComposite() throws Exception {
+        visitor = new CompositeUnihanVisitor(visitor1, visitor2);
+    }
+
+    @Test
+    public void should_visit_children_passed_in_constructor() throws Exception {
+        Hanzi hanzi = new Hanzi.Builder(0x3400).build();
+        visitor.visit(hanzi);
+
+        new Verifications() {
+            {
+                visitor1.visit(hanzi);
+                visitor2.visit(hanzi);
+            }
+        };
+    }
+
+    @Test
+    public void should_close_children_passed_in_constructor() throws Exception {
+        visitor.close();
+
+        new Verifications() {
+            {
+                visitor1.close();
+                visitor2.close();
+            }
+        };
+    }
+}
