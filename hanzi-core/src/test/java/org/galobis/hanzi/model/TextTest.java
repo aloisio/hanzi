16153c37837a34d506e72a2460ff18b77ec19d81
Aloisio Ribeiro <aloisio@users.noreply.github.com>
2015-12-31 21:27:56 -0500

    Include additional test cases for UNKNOWN Script

--- hanzi-core/src/test/java/org/galobis/hanzi/model/TextTest.java
+++ hanzi-core/src/test/java/org/galobis/hanzi/model/TextTest.java
@@ -1,6 +1,7 @@
 package org.galobis.hanzi.model;
 
 import static com.shazam.shazamcrest.MatcherAssert.assertThat;
+import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
@@ -9,7 +10,10 @@ import org.junit.Test;
 public class TextTest {
 
     @Test
-    public void should_return_UNKNOWN_for_null_String() {
-        assertThat(new Text(null).script(), is(equalTo(Script.UNKNOWN)));
+    public void should_return_UNKNOWN_for_non_Chinese_text() {
+        asList(null, "", " ", "\t", "\n", "ABC", "。", ".", "робот", "robô",
+                "1234", "ロボット", "로봇", "ρομπότ", "रोबोट").stream().forEach(
+                        input -> assertThat(new Text(input).script(),
+                                is(equalTo(Script.UNKNOWN))));
     }
 }
